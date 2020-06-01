d3606762de5d272dd5c176b2247e54d1189c0b2b
vovBorya <vovbor02@gmail.com>
2020-06-01 17:58:51 +0300

    refactored to ES6

--- src/game.js
+++ src/game.js
@@ -1,51 +1,49 @@
-var gameImgDir = "img/";
-var gTexts = []; // Для многоязычности.
+let gameImgDir = "img/";
+let gTexts = []; // Для многоязычности.
 
 gTexts['win1'] = 'Win ';
 gTexts['win2'] = '!';
 
-var gField = []; // игровое поле
+let gField = []; // игровое поле
 
-function createField(w, h) { // Задаёт размер игрового поля.
+const createField = (w, h) => { // Задаёт размер игрового поля.
   gField = new Array(w); // создание нового массива.
-  for (i = 0; i < w; i++) gField[i] = new Array(h); // теперь переделаем массив в матрицу.
-  var hT = "<table cellpadding='0' cellspacing='0'>"; // заголовок тега таблицы.
+  for (let i = 0; i < w; i++) gField[i] = new Array(h); // теперь переделаем массив в матрицу.
+  let hT = "<table cellpadding='0' cellspacing='0'>"; // заголовок тега таблицы.
 
-  for (j = 0; j < h; j++) {
+  for (let j = 0; j < h; j++) {
     hT += "<tr>"; // создание новой линии
-    for (i = 0; i < w; i++) { // вставка ячеек в линию
+    for (let i = 0; i < w; i++) { // вставка ячеек в линию
       hT += "<td>";
       hT += "<img height='60px' id='c" + i + "_" + j + "' src='" + gameImgDir + "c_null.png' alt=' ' onclick='onCellClk(" + i + "," + j + ")'>";
       hT += "</td>";
     }
     hT += "</tr>"; // конец линии
   }
-  console.log('create field')
-  //document.querySelector('#game').innerHTML = hT + "</table>"; // вставка таблицы на страницу.
-  document.getElementById("game").innerHTML = hT + "</table>"
-  /*document.getElementById('gameinfo').innerText = gTexts['start']; // отобразить сообщение*/
+
+  document.getElementById("game").innerHTML = hT + "</table>"; // вставка таблицы на страницу.
 }
 
-function setCell(x, y, t) { // Поставить крестик или нолик
+const setCell = (x, y, t) => { // Поставить крестик или нолик
   gField[x][y] = t; // Запомнить t в массиве
-  var imgsrc = gameImgDir + 'c_null.gif'; // изображение по умолчанию
-  if (t === 'x') imgsrc = gameImgDir + 'x.png'; // картинка для крестика
-  if (t === 'o') imgsrc = gameImgDir + 'o.png'; // картинка для нолика
-  var oName = "c" + x + "_" + y; // составление имени картинки
-  document.getElementById(oName).src = imgsrc; // замена изображения
+  let imgSrc = gameImgDir + 'c_null.gif'; // изображение по умолчанию
+  if (t === 'x') imgSrc = gameImgDir + 'x.png'; // картинка для крестика
+  if (t === 'o') imgSrc = gameImgDir + 'o.png'; // картинка для нолика
+  let oName = "c" + x + "_" + y; // составление имени картинки
+  document.getElementById(oName).src = imgSrc; // замена изображения
   document.getElementById(oName).width = 60;
   document.getElementById(oName).height = 60;
   if (t != null) document.getElementById(oName).alt = t; // если картинки выключены, то игра будет в текстовом режиме
 
 }
 
-function isWin() { // Проверка победы.
-  for (stX = 0; stX <= gField.length - 3; stX++)
-    for (stY = 0; stY <= gField[0].length - 3; stY++) { // Если размер поля больше трёх.
-      var lC = gField[stX][stY]; // проверка линии
+const isWin = () => { // Проверка победы.
+  for ( let stX = 0; stX <= gField.length - 3; stX++)
+    for ( let stY = 0; stY <= gField[0].length - 3; stY++) { // Если размер поля больше трёх.
+      let lC = gField[stX][stY]; // проверка линии
 
       if (lC != null) {
-        for (i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) {
           if (gField[i + stX][i + stY] !== lC) {
             lC = null; // если проверяемая клетка не содержит lC, то сбросить значение lC
           }
@@ -55,22 +53,22 @@ function isWin() { // Проверка победы.
 
       lC = gField[2 + stX][stY];
       if (lC != null)
-        for (i = 0; i < 3; i++)
+        for ( let i = 0; i < 3; i++)
           if (gField[2 - i + stX][i + stY] !== lC) lC = null;
       if (lC != null) return lC;
 
 
-      for (i = 0; i < 3; i++) { // проверка по вертикали
+      for ( let i = 0; i < 3; i++) { // проверка по вертикали
         lC = gField[stX + i][stY];
         if (lC != null)
-          for (j = 0; j < 3; j++)
+          for (let j = 0; j < 3; j++)
             if (gField[i + stX][j + stY] !== lC) lC = null;
         if (lC != null) return lC;
       }
-      for (j = 0; j < 3; j++) { // проверка по горизонтали
+      for (let j = 0; j < 3; j++) { // проверка по горизонтали
         lC = gField[stX][stY + j];
         if (lC != null)
-          for (i = 0; i < 3; i++)
+          for (let i = 0; i < 3; i++)
             if (gField[i + stX][j + stY] !== lC) lC = null;
         if (lC != null) return lC;
       }
@@ -80,16 +78,16 @@ function isWin() { // Проверка победы.
 
 function CompGame() { // ИИ
 
-  var tx = null,
+  let tx = null,
     ty = null,
     tp = 0; // tp - приоритет выбранной целевой клетки.
 
-  var stX = 0,
+  let stX = 0,
     stY = 0;
   for (stX = 0; stX < gField.length; stX++)
     for (stY = 0; stY < gField[0].length; stY++) // для каждой клетки
     {
-      var lC = gField[stX][stY];
+      let lC = gField[stX][stY];
       if ((lC !== 'x') && (lC !== 'o')) { // только для пустых клеток
 
         gField[stX][stY] = 'x';
@@ -104,7 +102,7 @@ function CompGame() { // ИИ
             ty = stY;
             tp = 2;
           } else if (tp < 2) { // или...
-            var mini = -1,
+            let mini = -1,
               maxi = 1,
               minj = -1,
               maxj = 1;
@@ -113,8 +111,8 @@ function CompGame() { // ИИ
             if (stX < 1) mini = 0;
             if (stY < 1) minj = 0;      // найти ближайший нолик...
 
-            for (i = mini; i <= maxi; i++)
-              for (j = minj; j <= maxj; j++)
+            for (let i = mini; i <= maxi; i++)
+              for (let j = minj; j <= maxj; j++)
                 if ((i !== 0) && (j !== 0)) { // если есть рядом своя занятая клетка - поближе к своим
                   if (gField[stX + i][stY + j] === 'o') {
                     tx = stX;
@@ -136,10 +134,10 @@ function CompGame() { // ИИ
   }
 }
 
-function onCellClk(x, y) { // Действия при клике по клетке
+const onCellClk = (x, y) => { // Действия при клике по клетке
   if (gField[x][y] == null) { // если клетка пустая
 
-    var win = isWin(); // проверка на победу.
+    let win = isWin(); // проверка на победу.
 
     if (!win) setCell(x, y, 'x');
     win = isWin(); // проверка на победу
@@ -147,18 +145,20 @@ function onCellClk(x, y) { // Действия при клике по клетк
       CompGame(); // запуск компьютерного игрока
       win = isWin(); // проверка на победу
     }
-    if (!win) {
-      gameinfo.innerText = gTexts['playing'] // отображение сообщения
-    } else {
-      var mes = gTexts['win1'] + win + gTexts['win2'];
+    if (win) {
+      let mes = gTexts['win1'] + win + gTexts['win2'];
       alert(mes); // отображение сообщения о победе
-      gameinfo.innerText = mes;
     }
   }
 }
 
-let n = 4;
-createField(n, n);
-function initGame() { // инициализация игры
+//очищаем поле
+const clearField = () => {
+  document.getElementById("game").innerHTML = null;//стираем ранее нарисованое поле
+  initGame();// рисуем новое
+}
+
+const initGame = () => { // инициализация игры
+  let n = 4;
   createField(n, n); // создание игрового поля
 }
